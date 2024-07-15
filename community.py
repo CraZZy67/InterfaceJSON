2ae6aa37604e7f7b1e3df2885dc3f4efb6e72127
Sergey <128322450+CraZZy67@users.noreply.github.com>
2024-07-15 15:29:18 +0300

    Закончил основную логику интерфейса для базы данных json.
    Код не протестирован и не обработан случай с пустым json.

--- community.py
+++ community.py
@@ -1,3 +1,50 @@
+from string import ascii_letters, digits
+from json import load, dump
+from random import shuffle
+
+
 class Community:
-    def __init__(self, content: dict):
-        self.__content = content
+    def __init__(self, json_file: str):
+        with open(json_file) as f:
+            self.__content = load(f)
+
+    def add_post(self, title: str, continue_link: str):
+        self.__content[self.gen_link_and_id()[0]] = [title, self.gen_link_and_id()[1], continue_link]
+
+    def gen_link_and_id(self) -> list:
+        list_chars = list()
+        for i in str(ascii_letters + digits): list_chars.append(i)
+
+        shuffle(list_chars)
+        if "".join(list_chars[0:6]) in self.__content["id_list"]:
+            shuffle(list_chars)
+
+        id_ = "".join(list_chars[0:6])
+        self.__content["id_list"].append(id_)
+        return [id_, f"https://vk.com/im?sel=-226581842&id={id_}"]
+
+    def delete_post(self, id_: str) -> str:
+        try:
+            self.__content.pop(id_)
+            return id_
+        except KeyError as ex:
+            return f"Не правильный id поста: {ex}"
+
+    def get_post_link(self, id_: str, type_: str) -> str:
+        if type_ == "id":
+            return self.__content[id_][1]
+        else:
+            return self.__content[id_][2]
+
+    def get_list_posts(self) -> str:
+        str_list = ""
+        for i, k in self.__content.items():
+            if i == "id_list":
+                break
+            str_list += f"{i}: {k[0]} | {k[1]} | {k[2]}\n"
+
+        return str_list
+
+    def save_to_json(self):
+        with open('posts.json', 'w') as f:
+            dump(self.__content, f, indent=4)
